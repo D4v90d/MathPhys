ae41da15e011e0d6cc32cc252302da73f7823a7d
David Koswari <42827497+D4v90d@users.noreply.github.com>
2019-06-27 00:45:34 +0700

    ToDo3

--- Meeting03_Dribble/Dribble.java
+++ Meeting03_Dribble/Dribble.java
@@ -58,6 +58,22 @@ public class Dribble {
         textField = new JTextField("Ball Color");
         textField.setBounds(10, 230, 95, 30);
         
+        JFrame corFrame = new JFrame("Coefficient of Restitution");
+        corFrame.setBounds(350,30,150,30);
+        f.add(corFrame);
+
+        JLabel info = new JLabel("If empty, COR will remain at 0.7");
+        info.setBounds(430,70,400,50);
+        f.add(info);
+
+        JLabel info2 = new JLabel("COR change will take effect on the next ball");
+        info2.setBounds(403,90,430,50);
+        f.add(info2);
+
+        JTextField corValue = new JTextField();
+        corValue.setBounds(550,30,100,30);
+        f.add(corValue);
+        
         frame.add(ballRadiusLabel);
         frame.add(ballPosXLabel);
         frame.add(ballPosYLabel);
@@ -68,12 +84,33 @@ public class Dribble {
         JButton button = new JButton("Generate Ball");
         button.setBounds(300, 300, 100, 20);
 
-        button.addActionListener(new ActionListener()){
-            
+        JButton def = new JButton("Spawn default balls");
+        def.setBounds(420,300,200,20);
+        def.addActionListener(new DefaultSpawn());
+
+        JButton kill = new JButton("Kill a ball");  //Ball killer
+        kill.setBounds(300,300,100,20);
+        kill.addActionListener(new CustomActionListener());
+       
+        //Listener to generate a new ball
+        b.addActionListener(new ActionListener()
+        {
+            @Override
             public void actionPerformed(ActionEvent e)
             {
-                Color color2 = Color.black; // default color
-                
+                System.out.println(corValue.getText().isEmpty());   //Test output to detect COR field change
+                //If COR field is filled
+                if (corValue.getText().isEmpty() == false)
+                {
+                    newCor = Double.parseDouble(corValue.getText());    //Get new COR value from the COR field
+                    if (newCor != 0.7 && newCor >= 0.0 && newCor <= 100.0)
+                    {
+                        cor = newCor;   //Set current COR with the new COR
+                    }
+                }
+                double ballCounter = balls.size();
+                System.out.println(ballCounter);
+                Color color2 = Color.black; 
                 double radius = Double.parseDouble(radius.getText());
                 double posX = Double.parseDouble(positionX.getText());
                 double posY = Double.parseDouble(positionY.getText());
@@ -100,20 +137,25 @@ public class Dribble {
                     case "orange":
                         color2 = color.orange;
                         break;    
-                    case "grey":
-                        color2 = color.grey;
+                    case "gray":
+                        color2 = color.gray;
                         break;
                 }
-
-
-
 
                 balls.add(new Ball(radius, posX, posY, velX, velY, color2));
             }
         };
         
-        JButton kill = new JButton("Delete Ball");
-        kill.setBounds(450,300,100,20);
+        f.add(b);
+        f.add(kill);
+        f.add(def);
+
+        f.setSize(700,400);
+        f.setLayout(null);
+        f.setVisible(true);
+       
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
 
         // create the walls
         createWalls();
