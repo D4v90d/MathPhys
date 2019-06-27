8bc4fcf94f3d5e52a0e6acb7dd2fa5c9609f78ae
David Koswari <42827497+D4v90d@users.noreply.github.com>
2019-06-27 07:54:30 +0700

    Hole Detection

--- Meeting06_Billiard/Ball.java
+++ Meeting06_Billiard/Ball.java
@@ -1,13 +1,12 @@
 package Meeting06_Billiard;
 
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Ball {
 	private double positionX;              // center of ball's position
 	private double positionY;
-	public final static double RADIUS = 30;
+	private double radius;
 	private double velocityX = 0;          // ball's velocity
 	private double velocityY = 0;
 	private final static double e = 1;     // ball's coefficient of resistution
@@ -15,9 +14,11 @@ public class Ball {
 	private Color ballColor;
 
 	public Ball(double positionX, double positionY, Color ballColor) {
+		this.radius = radius;
 		this.positionX = positionX;
 		this.positionY = positionY;
 		this.ballColor = ballColor;
+		this.mass = mass;
 	}
 
 	public double getPositionX() {
@@ -70,4 +71,88 @@ public class Ball {
 		double distanceY = this.positionY - other.getPositionY();
 		return Math.sqrt(distanceX * distanceX + distanceY * distanceY);
 	}
+	
+	public void ballCollide(ArrayList<Ball> balls) {
+        for (Ball b: balls) {
+            if (b != this && this.distance(b) <= this.radius + b.getRadius()) {
+
+                /*
+                if (this.mass == 1.0)
+                {
+                    balls.remove(b);
+                }
+                else if(b.mass == 1.0)
+                {
+                    balls.remove(this);
+                }
+                */
+                else
+                {
+                    //calculate the unit centre vector, that is the vector between the centre of the two balls colliding
+                    double CVectorX = b.getPositionX() - this.positionX;
+                    double CVectorY = b.getPositionY() - this.positionY;
+                    double CVectorLength = Math.sqrt(CVectorX*CVectorX + CVectorY*CVectorY);
+                    CVectorX = CVectorX/CVectorLength;
+                    CVectorY = CVectorY/CVectorLength;
+
+                    //calculate the normal vector of the centre vector (by rotating 90 degrees)
+                    double NVectorX = -CVectorY;
+                    double NVectorY =  CVectorX;
+
+                    //projecting the ball's velocity to the centre and normal vector of the two centre balls
+                    double V1x  = CVectorX * this.velocityX + CVectorY * this.velocityY;
+                    double V1y  = NVectorX * this.velocityX + NVectorY * this.velocityY;
+
+                    double V2x  = CVectorX * b.getVelocityX() + CVectorY * b.getVelocityY();
+                    double V2y = NVectorX * b.getVelocityX() + NVectorY * b.getVelocityY();
+
+                    //calculate the new velocity after collision
+                    double NewV1x = ((this.mass * V1x) + (b.getMass() * V2x) + b.getMass() * e *(V2x - V1x))/(this.mass + b.getMass());
+                    double NewV1y = V1y ;
+
+                    double NewV2x = ((this.mass * V1x) + (b.getMass() * V2x) - this.mass * e * (V2x - V1x))/(this.mass + b.getMass());
+                    double NewV2y = V2y ;
+
+                    //set the new velocity for the two balls colliding
+
+                    this.velocityX = NewV1x * CVectorX + NewV1y * NVectorX;
+                    this.velocityY = NewV1x * CVectorY + NewV1y * NVectorY;
+
+                    b.setVelocityX(NewV2x * CVectorX + NewV2y * NVectorX);
+                    b.setVelocityY(NewV2x * CVectorY + NewV2y * NVectorY);
+
+                    while(this.distance(b) <= (this.radius + b.getRadius()))
+                    {
+                        this.positionX += velocityX;
+                        this.positionY -= velocityY;
+                        b.setPositionX(b.getPositionX() + b.getVelocityX());
+                        b.setPositionX(b.getPositionX() - b.getVelocityY());
+                    }
+                }
+            }
+        }
+    }
+
+    // check collision between walls and the ball
+    public void wallCollide(ArrayList<Wall> walls) {
+        for (Wall w : walls) {
+            if(w.distanceFromPoint(positionX, positionY) <= radius) {
+                double error = radius - w.distanceFromPoint(positionX, positionY);
+
+                positionX += error * w.normalLine().getX();
+                positionY -= error * w.normalLine().getY();
+
+                //if the ball collided with a vertical wall
+                if(w.getWidth() == 0) {
+                    velocityX *= -1;
+                }
+
+                //if the ball collided with a horizontal wall
+                else if(w.getHeight() == 0) {
+                    velocityY *= -1;
+                }
+            }
+        }
+    }
+}
 }
